59098581c202a307da267eaa03ea9b4ac4976db7
AsaOfDiamonds <asaofdiamonds@gmail.com>
2019-05-23 15:41:09 -0600

    first commit first react native app done

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is Awesome</Text>
+        <Text>Go Space Wizards!!</Text>
+        <Text>built with React Native</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+});
